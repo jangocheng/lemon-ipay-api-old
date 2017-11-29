2c87738060d8b0c5c277f6f0a57df04b0c45cc79
xiao.xinmiao <xiaoxm_001@163.com>
2017-11-29 14:08:25 +0800

    add query tag for prepayeasy

--- wechat/dto.go
+++ wechat/dto.go
@@ -31,7 +31,7 @@ type ReqPrepayDto struct {
 
 type ReqPrepayEasyDto struct {
 	*wxpay.ReqPrepayDto
-	EId   int64  `json:"e_id"`
+	EId   int64  `json:"e_id" query:"e_id"`
 	AppId string `json:"app_id" query:"app_id"` //required
 	Scope string `json:"scope" query:"scope"`   //option
 	State string `json:"state" query:"state"`   //option
