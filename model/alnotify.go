afe1ec3d492e8000e773666e0779262f7640c8b4
xiaoxinmiao <xiaoxm_001@163.com>
2017-12-07 10:44:15 +0800

    query => form

--- model/alnotify.go
+++ model/alnotify.go
@@ -6,39 +6,39 @@ import (
 )
 
 type NotifyAlipay struct {
-	NotifyTime string `json:"notify_time,omitempty" query:"notify_time,omitempty"`
-	NotifyType string `json:"notify_type,omitempty" query:"notify_type,omitempty"`
-	NotifyId   string `json:"notify_id,omitempty" query:"notify_id,omitempty"`
-	SignType   string `json:"sign_type,omitempty" query:"sign_type,omitempty"`
-	Sign       string `json:"sign,omitempty"  query:"sign,omitempty" xorm:"varchar(256)"`
+	NotifyTime string `json:"notify_time,omitempty" form:"notify_time,omitempty"`
+	NotifyType string `json:"notify_type,omitempty" form:"notify_type,omitempty"`
+	NotifyId   string `json:"notify_id,omitempty" form:"notify_id,omitempty"`
+	SignType   string `json:"sign_type,omitempty" form:"sign_type,omitempty"`
+	Sign       string `json:"sign,omitempty"  form:"sign,omitempty" xorm:"varchar(256)"`
 
-	TradeNo    string `json:"trade_no,omitempty" query:"trade_no,omitempty"`
-	AppId      string `json:"app_id,omitempty" query:"app_id,omitempty" xorm:"appid"`
-	OutTradeNo string `json:"out_trade_no,omitempty" query:"out_trade_no,omitempty"`
-	OutBizNo   string `json:"out_biz_no,omitempty" query:"out_biz_no,omitempty"`
-	BuyerId    string `json:"buyer_id,omitempty" query:"buyer_id,omitempty"`
+	TradeNo    string `json:"trade_no,omitempty" form:"trade_no,omitempty"`
+	AppId      string `json:"app_id,omitempty" form:"app_id,omitempty" xorm:"appid"`
+	OutTradeNo string `json:"out_trade_no,omitempty" form:"out_trade_no,omitempty"`
+	OutBizNo   string `json:"out_biz_no,omitempty" form:"out_biz_no,omitempty"`
+	BuyerId    string `json:"buyer_id,omitempty" form:"buyer_id,omitempty"`
 
-	BuyerLogonId string `json:"buyer_logon_id,omitempty" query:"buyer_logon_id,omitempty"`
-	SellerId     string `json:"seller_id,omitempty" query:"seller_id,omitempty"`
-	SellerEmail  string `json:"seller_email,omitempty" query:"seller_email,omitempty"`
-	TradeStatus  string `json:"trade_status,omitempty" query:"trade_status,omitempty"`
-	TotalAmount  string `json:"total_amount,omitempty" query:"total_amount,omitempty"` //float64
+	BuyerLogonId string `json:"buyer_logon_id,omitempty" form:"buyer_logon_id,omitempty"`
+	SellerId     string `json:"seller_id,omitempty" form:"seller_id,omitempty"`
+	SellerEmail  string `json:"seller_email,omitempty" form:"seller_email,omitempty"`
+	TradeStatus  string `json:"trade_status,omitempty" form:"trade_status,omitempty"`
+	TotalAmount  string `json:"total_amount,omitempty" form:"total_amount,omitempty"` //float64
 
-	ReceiptAmount  string `json:"receipt_amount,omitempty" query:"receipt_amount,omitempty"`     //float64
-	InvoiceAmount  string `json:"invoice_amount,omitempty" query:"invoice_amount,omitempty"`     //float64
-	BuyerPayAmount string `json:"buyer_pay_amount,omitempty" query:"buyer_pay_amount,omitempty"` //float64
-	PointAmount    string `json:"point_amount,omitempty" query:"point_amount,omitempty"`         //float64
-	RefundFee      string `json:"refund_fee,omitempty" query:"refund_fee,omitempty"`             //float64
+	ReceiptAmount  string `json:"receipt_amount,omitempty" form:"receipt_amount,omitempty"`     //float64
+	InvoiceAmount  string `json:"invoice_amount,omitempty" form:"invoice_amount,omitempty"`     //float64
+	BuyerPayAmount string `json:"buyer_pay_amount,omitempty" form:"buyer_pay_amount,omitempty"` //float64
+	PointAmount    string `json:"point_amount,omitempty" form:"point_amount,omitempty"`         //float64
+	RefundFee      string `json:"refund_fee,omitempty" form:"refund_fee,omitempty"`             //float64
 
-	SendBackFee string `json:"send_back_fee,omitempty" query:"send_back_fee,omitempty"` //float64
-	Subject     string `json:"subject,omitempty" query:"subject,omitempty" xorm:"varchar(256)"`
-	Body        string `json:"body,omitempty" query:"body,omitempty" xorm:"varchar(400)"`
-	GmtCreate   string `json:"gmt_create,omitempty" query:"gmt_create,omitempty"`
-	GmtPayment  string `json:"gmt_payment,omitempty" query:"gmt_payment,omitempty"`
+	SendBackFee string `json:"send_back_fee,omitempty" form:"send_back_fee,omitempty"` //float64
+	Subject     string `json:"subject,omitempty" form:"subject,omitempty" xorm:"varchar(256)"`
+	Body        string `json:"body,omitempty" form:"body,omitempty" xorm:"varchar(400)"`
+	GmtCreate   string `json:"gmt_create,omitempty" form:"gmt_create,omitempty"`
+	GmtPayment  string `json:"gmt_payment,omitempty" form:"gmt_payment,omitempty"`
 
-	GmtRefund    string    `json:"gmt_refund,omitempty" query:"gmt_refund,omitempty"`
-	GmtClose     string    `json:"gmt_close,omitempty" query:"gmt_close,omitempty"`
-	FundBillList string    `json:"fund_bill_list,omitempty" query:"fund_bill_list,omitempty" xorm:"varchar(512)"`
+	GmtRefund    string    `json:"gmt_refund,omitempty" form:"gmt_refund,omitempty"`
+	GmtClose     string    `json:"gmt_close,omitempty" form:"gmt_close,omitempty"`
+	FundBillList string    `json:"fund_bill_list,omitempty" form:"fund_bill_list,omitempty" xorm:"varchar(512)"`
 	CreatedAt    time.Time `json:"created_at" xorm:"created"`
 }
 
