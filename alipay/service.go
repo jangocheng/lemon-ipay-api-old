64b354697de655aeaacde0517343bdd5b7dc9b16
xiaoxinmiao <xiaoxm_001@163.com>
2017-12-09 12:03:43 +0800

    refactor code and test

---
+++ alipay/service.go
@@ -0,0 +1,69 @@
+package alipay
+
+import (
+	"errors"
+	"lemon-ipay-api/model"
+	"strconv"
+
+	"github.com/relax-space/go-kit/base"
+	"github.com/relax-space/go-kit/sign"
+	alpay "github.com/relax-space/lemon-alipay-sdk"
+)
+
+func QueryCommon(account model.AlAccount, outTradeNo string) (result *alpay.RespQueryDto, err error) {
+	reqDto := ReqQueryDto{}
+	reqDto.ReqBaseDto = &alpay.ReqBaseDto{
+		AppId:        account.AppId,
+		AppAuthToken: account.AuthToken,
+	}
+
+	customDto := &alpay.ReqCustomerDto{
+		PriKey: account.PriKey,
+		PubKey: account.PubKey,
+	}
+	reqDto.OutTradeNo = outTradeNo
+	result, err = alpay.Query(reqDto.ReqQueryDto, customDto)
+	return
+}
+
+func ValidNotify(body, signParam, outTradeNo, totalAmount string, mapParam map[string]interface{}) (err error) {
+	//0.get account info
+	bodyMap := base.ParseMapObjectEncode(body)
+	var eId int64
+	var flag bool
+	if eIdObj, ok := bodyMap["e_id"]; ok {
+		if eId, err = strconv.ParseInt(eIdObj.(string), 10, 64); err == nil {
+			flag = true
+		}
+	}
+	if !flag {
+		err = errors.New("e_id(int64) is not existed in param(param name:body) or format is not correct")
+		return
+	}
+
+	account, err := model.AlAccount{}.Get(eId)
+	if err != nil {
+		return
+	}
+
+	//1.valid sign
+	signStr := signParam
+	delete(mapParam, "sign")
+	delete(mapParam, "sign_type")
+
+	if !sign.CheckSha1Sign(base.JoinMapObjectEncode(mapParam), signStr, account.PubKey) {
+		err = errors.New("sign valid failure")
+		return
+	}
+
+	//2.valid data
+	queryDto, err := QueryCommon(account, outTradeNo)
+	if err != nil {
+		return
+	}
+	if !(queryDto.TotalAmount == totalAmount) {
+		err = errors.New("amount is exception")
+		return
+	}
+	return
+}
